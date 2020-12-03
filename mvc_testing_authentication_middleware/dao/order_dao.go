3190c7e44f1ac5728be3b04976adee2f2238ebd6
hima-del <himajaharidas77@gmail.com>
2020-12-04 00:53:36 +0530

    order

--- mvc_testing_authentication_middleware/dao/order_dao.go
+++ mvc_testing_authentication_middleware/dao/order_dao.go
@@ -8,6 +8,17 @@ func QueryFifteen(extrctedID int, billingAddress, shippingAddress string) (err e
 }
 
 func QuerySixteen(extrctedID int, orderDate, shippingDate, shippingAddress, orderStatus string) (err error) {
-	_, err = config.DB.Query("insert into orders (customer id,order date,ship date,shipping address, order status)values ($1,$2,$3)", extrctedID, orderDate, shippingDate, shippingAddress, orderStatus)
+	_, err = config.DB.Query("insert into orders (customer id,order date,ship date,shipping address, order status)values ($1,$2,$3,$4,$5)", extrctedID, orderDate, shippingDate, shippingAddress, orderStatus)
+	return err
+}
+
+func QuerySeventeen(extractedID int) (id int, err error) {
+	result := config.DB.QueryRow("select id from orders where customer id=$1", extractedID)
+	err = result.Scan(&id)
+	return id, err
+}
+
+func QueryEighteen(orderID int, price, discount, total float64, quantity int, color, size string) (err error) {
+	_, err = config.DB.Query("insert into order_details (order id, price, discount, total,quantity,color,size)values ($1,$2,$3,$4,$5,$6,$7)", orderID, price, discount, total, quantity, color, size)
 	return err
 }

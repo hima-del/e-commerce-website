9b5ed4d8a220882e13ecf5b510736d6104e754a0
hima-del <himajaharidas77@gmail.com>
2020-12-04 16:25:13 +0530

    create order

--- mvc_testing_authentication_middleware/database/migration/000001_init_schema.up.sql
+++ mvc_testing_authentication_middleware/database/migration/000001_init_schema.up.sql
@@ -1,37 +1,37 @@
  CREATE TABLE "customers"("id" bigserial PRIMARY KEY,
  "username" TEXT NOT NULL,
  "password"TEXT NOT NULL,
- "created_at" timestamptz NOT NULL DEFAULT (now())
+ "createdat" timestamptz NOT NULL DEFAULT (now())
  );
 
  CREATE TABLE "address"("id"bigserial PRIMARY KEY,
- "customer id"INT NOT NULL,
- "billing address" TEXT NOT NULL,
- "shipping address"TEXT NOT NULL UNIQUE,
- "created_at" timestamptz NOT NULL DEFAULT (now())
+ "customerid"INT NOT NULL,
+ "billingaddress" TEXT NOT NULL,
+ "shippingaddress"TEXT NOT NULL UNIQUE,
+ "createdat" timestamptz NOT NULL DEFAULT (now())
  );
 
 
  CREATE TABLE "orders"("id"bigserial PRIMARY KEY,
- "customer id"INT NOT NULL,
- "order date" DATE NOT NULL,
- "ship date"DATE NOT NULL,
- "shipping address"TEXT NOT NULL,
- "order status"TEXT NOT NULL,
- "created_at" timestamptz NOT NULL DEFAULT (now())
+ "customerid"INT NOT NULL,
+ "orderdate" DATE NOT NULL,
+ "shipdate"DATE NOT NULL,
+ "shippingaddress"TEXT NOT NULL,
+ "orderstatus"TEXT NOT NULL,
+ "createdat" timestamptz NOT NULL DEFAULT (now())
  );
 
- CREATE TABLE "order_details"("id"bigserial PRIMARY KEY,
- "product id"INT NOT NULL,
- "order id"INT NOT NULL,
- "order number"serial NOT NULL,
+ CREATE TABLE "orderdetails"("id"bigserial PRIMARY KEY,
+ "productid"INT NOT NULL,
+ "orderid"INT NOT NULL,
+ "ordernumber"serial NOT NULL,
  "price"FLOAT NOT NULL,
  "discount"FLOAT NOT NULL,
  "total"FLOAT NOT NULL,
  "quantity"INT NOT NULL,
  "color"TEXT NOT NULL,
  "size"TEXT NOT NULL,
- "created_at" timestamptz NOT NULL DEFAULT (now())
+ "createdat" timestamptz NOT NULL DEFAULT (now())
  );
 
 
@@ -39,19 +39,19 @@
  "name"TEXT NOT NULL,
  "price"FLOAT NOT NULL,
  "picture"TEXT NOT NULL,
- "created_at" timestamptz NOT NULL DEFAULT (now())
+ "createdat" timestamptz NOT NULL DEFAULT (now())
  );
  
 
  CREATE TABLE "blacklist"("token"TEXT NOT NULL);
 
-ALTER TABLE "address" ADD FOREIGN KEY ("customer id") REFERENCES "customers"("id");
+ALTER TABLE "address" ADD FOREIGN KEY ("customerid") REFERENCES "customers"("id");
 
-ALTER TABLE "orders" ADD FOREIGN KEY ("customer id") REFERENCES "customers"("id");
+ALTER TABLE "orders" ADD FOREIGN KEY ("customerid") REFERENCES "customers"("id");
 
-ALTER TABLE "order_details" ADD FOREIGN KEY ("product id") REFERENCES "products"("id");
+ALTER TABLE "orderdetails" ADD FOREIGN KEY ("productid") REFERENCES "products"("id");
 
-ALTER TABLE "order_details" ADD FOREIGN KEY ("order id") REFERENCES "orders"("id");
+ALTER TABLE "orderdetails" ADD FOREIGN KEY ("orderid") REFERENCES "orders"("id");
 
-ALTER TABLE "orders" ADD FOREIGN KEY ("shipping address") REFERENCES "address"("shipping address");
+ALTER TABLE "orders" ADD FOREIGN KEY ("shippingaddress") REFERENCES "address"("shippingaddress");
 
